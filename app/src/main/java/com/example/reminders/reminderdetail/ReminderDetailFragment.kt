50642f5f707fc3856d34e81689802eff15058328
dhruvit <bhavsar.dhruvit@gmail.com>
2020-09-28 16:02:32 -0400

    remove the keyboard handling

--- app/src/main/java/com/example/reminders/reminderdetail/ReminderDetailFragment.kt
+++ app/src/main/java/com/example/reminders/reminderdetail/ReminderDetailFragment.kt
@@ -1,11 +1,9 @@
 package com.example.reminders.reminderdetail
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -34,14 +32,6 @@ class ReminderDetailFragment : Fragment() {
             ViewModelProvider(this, viewModelFactory).get(ReminderDetailViewModel::class.java)
         binding.viewModel = viewModel
 
-        binding.reminderDetailTitle.setOnFocusChangeListener { _, hasFocus ->
-            if (!hasFocus) hideKeyboard()
-        }
-
-        binding.reminderDetailDescription.setOnFocusChangeListener { _, hasFocus ->
-            if (!hasFocus) hideKeyboard()
-        }
-
         viewModel.saveReminderClicked.observe(viewLifecycleOwner, {
             if (it) saveReminder()
         })
@@ -68,11 +58,5 @@ class ReminderDetailFragment : Fragment() {
                 binding.reminderDetailDescription.text.toString()
             )
         }
-    }
-
-    private fun hideKeyboard() {
-        val inputMethodManager =
-            context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 }
